11852c76974bf7fc8ac033c7011b0eaca36aa33e
Sarthaksingh2005 <sarthakash26@gmail.com>
2025-10-05 01:53:35 +0530

    created for traversal

---
+++ Traversal.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void DirectoryWalaker();
+{
+    //find files
+}
+void MeatadataCollection();
+{
+    //collect name,size,date of modification,file path, last modified
+}
+void PathHandler();
+{
+    //handes file path validity 
+}
+void errorhandling();
+{
+    //prepares up the code to handes complier if case of small minute errors..
+}
