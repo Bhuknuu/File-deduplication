74070b4659d054223e0e0f1fadf19719abeff94c
Anunay <anunaysharma88@gmail.com>
2025-10-05 01:44:25 +0530

    new file:   .gitignore 	new file:   Filters.h 	modified:   main.c

---
+++ Filters.h
@@ -0,0 +1,2 @@
+//Empty
+//Traveral functions
--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "Traversal.h"
+
+// THIS IS MAIN FILE OF FILE DEDUPLICATION PROJECT
+// IMPORTING ALL THE NECESSARY LIBRARIES AND HEADER FILES
+
+//User Interface functions
+//Filter functions
+//Progress bar function
+//Hashing functions
+//result display functions
+//Traversing functions
+
 
 int main(){
   printf("Hello, World!");
+  return 0;
 }
