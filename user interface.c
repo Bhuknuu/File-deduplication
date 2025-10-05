61dcd4fa06ccf884e3b1ffe933fd5d7400ae3589
VanshikaKritiSingh <vanshikakriti64@gmail.com>
2025-10-05 21:45:07 +0530

    Update user interface.c

--- user interface.c
+++ user interface.c
@@ -6,7 +6,7 @@ void display_main_menu()
   printf("\n FILE DEDUPLICATION SYSTEM\n");
   printf("1. Scan Directories\n");
   printf("2. Find Duplicate files\n");
-  printf("3. ");
+  printf("3. Configure Action\n");
   printf("4. Execute Action\n");
   printf("5. Exit\n");
   printf("Enter your choice:");
@@ -17,4 +17,18 @@ int get_user_choice()
   int ch;
   scanf("%d",&ch);
   return ch;
+}
+
+ActionType configure_action()
+{
+  printf("\nCONFIGURE ACTION\n");
+  printf("1. Delete duplicate files\n");
+  printf("2. Move duplicate files\n");
+  printf("3. Create hard links\n");
+  printf("4. Report only\n");
+  printf("5.Select Action:");
+  
+  int action;
+  scanf("%d",&action);
+  return(ActionType)(action-1);
 }
