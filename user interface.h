8d8cf0dbf6dd9d60a5fa76a4df855a7754706233
VanshikaKritiSingh <vanshikakriti64@gmail.com>
2025-10-05 20:05:59 +0530

    Create user interface.h

---
+++ user interface.h
@@ -0,0 +1,13 @@
+#ifndef USER INTERFACE_H
+#define USER INTERFACE_H
+
+#include "common.h"
+
+void display_main_menu();
+int get_user_choice();
+
+void show_progress(int current,int total);
+void show_error(const ch* message);
+void display_results(FileGroup* duplicates);
+
+#endif
