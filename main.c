85131f01a9b764c0fd4a893b0fdd9bfbab67fc04
Anunay Sharma <24021469@geu.ac.in>
2025-10-05 17:31:09 +0530

    made choice common.h and traversal.h

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-//#include "Traversal.h"
+#include "common.h"
+#include "traversal.h"
 
 // THIS IS MAIN FILE OF FILE DEDUPLICATION PROJECT
 // IMPORTING ALL THE NECESSARY LIBRARIES AND HEADER FILES
@@ -13,7 +11,50 @@
 //result display functions
 //Traversing functions
 
-int main(){
-  printf("Hello, World!");
-  return 0;
+int main() {
+    FileGroup all_files = {0};
+    FileGroup duplicates = {0};
+    ActionType action = ACTION_REPORT;
+    
+    while (true) {
+        display_main_menu();
+        
+        switch (get_user_choice()) {
+            case 1: // Scan directories
+                if (scan_directories(&all_files)) {
+                    printf("Scan completed! Total files found: %d.\n", all_files.count);
+                }
+                break;
+                
+            case 2: // Find duplicates
+                if (all_files.count > 0) {
+                    find_duplicates(&all_files, &duplicates);
+                    printf("Duplicate groups found: %d\n", duplicates.count);
+                } else {
+                    printf("Scan directories first!\n");
+                }
+                break;
+                
+            case 3: // Configure action
+                action = configure_action();
+                break;
+
+    
+            case 4: // Execute action
+                if (duplicates.count > 0) {
+                    execute_action(&duplicates, action);
+                } else {
+                    printf("Find duplicates first!\n");
+                }
+                break;
+                
+            case 5: // Exit
+                cleanup(&all_files, &duplicates);
+                printf("Exiting...\n");
+                return 0;
+                
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
 }
