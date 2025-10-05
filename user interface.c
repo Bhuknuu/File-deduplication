3b8a6d13105e32e1d9df758aa90d467f0bf95a20
VanshikaKritiSingh <vanshikakriti64@gmail.com>
2025-10-05 20:17:57 +0530

    Update user interface.c

--- user interface.c
+++ user interface.c
@@ -1,1 +1,20 @@
+#include "user interface.h"
+#include<string.h>
 
+void display_main_menu()
+{ 
+  printf("\n FILE DEDUPLICATION SYSTEM\n");
+  printf("1. Scan Directories\n");
+  printf("2. Find Duplicate files\n");
+  printf("3. ");
+  printf("4. Execute Action\n");
+  printf("5. Exit\n");
+  printf("Enter your choice:");
+}
+
+int get_user_choice()
+{
+  int ch;
+  scanf("%d",&ch);
+  return ch;
+}
